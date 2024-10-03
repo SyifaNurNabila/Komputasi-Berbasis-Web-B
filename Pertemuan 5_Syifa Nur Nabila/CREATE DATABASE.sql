755e8583b17c33346ec0723f45ef09bccdec0b9c
Syifa Nur Nabila <109137932+SyifaNurNabila@users.noreply.github.com>
2024-10-03 11:36:50 +0700

    Add files via upload

---
+++ Pertemuan 5_Syifa Nur Nabila/CREATE DATABASE.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE kontak;
+show databases;
+USE kontak;
+select * from kontak_kami;
+CREATE TABLE kontak_kami ( Nama varchar(100), Email varchar(100), Hobi varchar(100), Jenis_Kelamin varchar(100), Umur INT );
