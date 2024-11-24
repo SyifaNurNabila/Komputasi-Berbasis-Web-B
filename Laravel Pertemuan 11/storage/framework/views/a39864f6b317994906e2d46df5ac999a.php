c3e3b89573db80efb86aed46c36f06334b96a5bf
Syifa Nur Nabila <109137932+SyifaNurNabila@users.noreply.github.com>
2024-11-24 14:40:35 +0700

    Add files via upload

---
+++ Laravel Pertemuan 11/storage/framework/views/a39864f6b317994906e2d46df5ac999a.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+</head>
+<body>
+    <h1>INI HOME SYIFA </h1>
+    <nav>
+        <a href="<?php echo e(route('home')); ?>">Home</a>
+        <a href="<?php echo e(route('about')); ?>">About</a>
+        <a href="<?php echo e(route('contact')); ?>">Contact</a>
+    </nav>
+</body>
+</html><?php /**PATH C:\laragon\www\laravel11\resources\views/home.blade.php ENDPATH**/ ?>
